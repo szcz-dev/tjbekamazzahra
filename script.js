3a71dc8f49a259bc26ef7563fdc5bcb3be995c64
szcz-dev <tadatahari@gmail.com>
2025-03-02 14:09:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ AOS.init({
 const navbar = document.querySelector('.modern-navbar');
 
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset === 0) {
-    // At the very top, show the navbar
-    navbar.style.top = '0';
-  } else {
-    // Scrolled down, hide the navbar by moving it up out of view
-    navbar.style.top = `-${navbar.offsetHeight}px`;
-  }
+ if (window.pageYOffset === 0) {
+  // At the very top, show the navbar
+  navbar.style.top = '0';
+ } else {
+  // Scrolled down, hide the navbar by moving it up out of view
+  navbar.style.top = `-${navbar.offsetHeight}px`;
+ }
 });
-
